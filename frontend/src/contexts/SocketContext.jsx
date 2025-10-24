7f40c12437a6845e545a11478b0aad9015aac845
Bonakurthi Hitesh <93438836+Hitesh9878@users.noreply.github.com>
2025-10-24 20:53:23 +0530

    Update SocketContext.jsx

--- frontend/src/contexts/SocketContext.jsx
+++ frontend/src/contexts/SocketContext.jsx
@@ -28,7 +28,7 @@ export const SocketProvider = ({ children }) => {
         return;
       }
 
-      const newSocket = io('http://localhost:5000', {
+      const newSocket = io('https://chat-application-fj04.onrender.com', {
         auth: { token },
         reconnection: true,
         reconnectionDelay: 1000,
